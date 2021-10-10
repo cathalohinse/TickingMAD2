49141fab56fa406404dfdb250bf9dfa31bd6d943
Cathal Henchy <20091405@mail.wit.ie>
2021-10-10 11:02:32 +0100

    no message

--- app/src/main/java/org/wit/tickingmad/TickingmadActivity.kt
+++ app/src/main/java/org/wit/tickingmad/TickingmadActivity.kt
@@ -2,10 +2,14 @@ package org.wit.tickingmad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
-class TickingActivity : AppCompatActivity() {
+class TickingmadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ticking)
+        setContentView(R.layout.activity_tickingmad)
+        Timber.plant(Timber.DebugTree())
+        i("Tickingmad Activity started..")
     }
 }
