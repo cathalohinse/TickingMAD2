a0165c1e63c2adc45f59a13bc07e95a3eb501900
Cathal Henchy <20091405@mail.wit.ie>
2021-10-10 14:50:42 +0100

    Refactored files & packages (models & activities)

--- app/src/main/java/org/wit/tickingmad/activities/TickingmadActivity.kt
+++ app/src/main/java/org/wit/tickingmad/activities/TickingmadActivity.kt
@@ -1,25 +1,32 @@
-package org.wit.tickingmad
+package org.wit.tickingmad.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
+import org.wit.tickingmad.databinding.ActivityTickingmadBinding
+import org.wit.tickingmad.models.TickingmadModel
 import timber.log.Timber
 import timber.log.Timber.i
-import org.wit.tickingmad.databinding.ActivityTickingmadBinding
 
 class TickingmadActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityTickingmadBinding
+    var tickingmad = TickingmadModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityTickingmadBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setContentView(R.layout.activity_tickingmad)
+
         Timber.plant(Timber.DebugTree())
-        i("Tickingmad Activity started..")
+
+        i("Tickingmad Activity started...")
+
         binding.btnAdd.setOnClickListener() {
-            val tickTitle = binding.tickTitle.text.toString()
-            if (tickTitle.isNotEmpty()) {
-                i("add Button Pressed: $tickTitle")
+            tickingmad.title = binding.tickTitle.text.toString()
+            if (tickingmad.title.isNotEmpty()) {
+                i("add Button Pressed: $tickingmad.title")
             }
             else {
                 Snackbar
---
+++ app/src/main/java/org/wit/tickingmad/models/TickingmadModel.kt
@@ -0,0 +1,3 @@
+package org.wit.tickingmad.models
+
+data class TickingmadModel(var title: String = "")
