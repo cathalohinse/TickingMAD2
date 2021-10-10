ce92ddfa63e729223a5260689493a8d16a13cc7b
Cathal Henchy <20091405@mail.wit.ie>
2021-10-10 11:02:23 +0100

    Further updated layout & themes

--- app/src/main/java/org/wit/tickingmad/TickingmadActivity.kt
+++ app/src/main/java/org/wit/tickingmad/TickingmadActivity.kt
@@ -3,9 +3,9 @@ package org.wit.tickingmad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TickingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ticking)
     }
 }
