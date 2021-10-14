dcf070accc1b799b6e7665727305df5e6b35ea53
Cathal Henchy <20091405@mail.wit.ie>
2021-10-14 20:45:29 +0100

    Edit feature is now rendering on UI.

--- app/src/main/java/org/wit/tickingmad/activities/TickingmadListActivity.kt
+++ app/src/main/java/org/wit/tickingmad/activities/TickingmadListActivity.kt
@@ -59,5 +59,10 @@ class TickingmadListActivity : AppCompatActivity(), TickingmadListener {
         startActivityForResult(launcherIntent,0)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
 
