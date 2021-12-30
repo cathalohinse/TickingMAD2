956821a229978344258cdecc0199a2d66e03be9b
Cathal Henchy <20091405@mail.wit.ie>
2021-12-30 21:34:06 +0000

    Inserted placeholders for Firebase DB. An initial attmept was made to implement this, but it failed. I accidentally did it directly on the 'Develop' branch, instead of developing a purpose built branch

--- app/src/main/java/org/wit/tickingmad/models/TickingmadModel.kt
+++ app/src/main/java/org/wit/tickingmad/models/TickingmadModel.kt
@@ -2,10 +2,12 @@ package org.wit.tickingmad.models
 
 import android.net.Uri
 import android.os.Parcelable
+import com.google.firebase.database.Exclude
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class TickingmadModel(var id: Long = 0,
+data class TickingmadModel(var uid: String? = "",
+                           var id: Long = 0,
                            var _id: String = "N/A",
                            var email: String = "joe@bloggs.com",
                            var title: String = "",
@@ -21,3 +23,23 @@ data class TickingmadModel(var id: Long = 0,
 data class Location(var lat: Double = 0.0,
                     var lng: Double = 0.0,
                     var zoom: Float = 0f) : Parcelable
+
+/*{
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "uid" to uid,
+            "id" to id,
+            "_id" to _id,
+            "email" to email,
+            "title" to title,
+            "description" to description,
+            "county" to county,
+            "favourite" to favourite,
+            "image" to image,
+            "lat" to lat,
+            "lng" to lng,
+            "zoom" to zoom
+        )
+    }
+}*/
--- app/src/main/java/org/wit/tickingmad/models/TickingmadStore.kt
+++ app/src/main/java/org/wit/tickingmad/models/TickingmadStore.kt
@@ -1,6 +1,7 @@
 package org.wit.tickingmad.models
 
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseUser
 
 interface TickingmadStore {
     fun findAll(): List<TickingmadModel>
@@ -9,6 +10,19 @@ interface TickingmadStore {
     fun delete(tickingmad: TickingmadModel)
     fun findById(id:Long) : TickingmadModel?
 }
+
+/*interface TickingmadStore {
+    fun findAll(tickingmadsList:
+                MutableLiveData<List<TickingmadModel>>)
+    fun findAll(userid:String,
+                tickingmadsList:
+                MutableLiveData<List<TickingmadModel>>)
+    fun findById(userid:String, tickingmadid: String,
+                 tickingmad: MutableLiveData<TickingmadModel>)
+    fun create(firebaseUser: MutableLiveData<FirebaseUser>, tickingmad: TickingmadModel)
+    fun delete(userid:String, tickingmadid: String)
+    fun update(userid:String, tickingmadid: String, tickingmad: TickingmadModel)
+}*/
 
 /*interface TickingmadStore {
     fun findAll(tickingmadsList:
